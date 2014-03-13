8e248611828bd172cc67fc8e0da6514fe9acb253
isabelc <me@isabelcastillo.com>
2014-03-13 13:55:30 -0400

    Moved if have_posts check to section for regular archive loop so as to avoid conflict with other loops.Subheading sort-order remains optional. Fixes #8

--- includes/docs-template.php
+++ includes/docs-template.php
@@ -15,7 +15,6 @@ get_header(); ?>
 echo $Isa_Organized_Docs->organized_docs_section_heading();
 echo $Isa_Organized_Docs->organized_docs_content_nav();
 do_action( 'organized_docs_content_after_nav' ); 
-if ( have_posts() ) :
 	
 	// Display a list of subTerms, within a specified Term, AND show all the posts within each of those subTerms on archive page
 		
@@ -66,12 +65,19 @@ if ( have_posts() ) :
 		} else {
 		
 			// Not main docs page, and there are no child terms, do regular term loop to list posts within current term
+
+			if ( have_posts() ) :
 			echo '<ul>';
 			while ( have_posts() ) {
 				the_post();
 				echo '<li><a href="' . get_permalink($post->ID).'">' . get_the_title() . '</a></li>';		
 			}
 			echo '</ul>';
+	
+	 		else : ?>
+			<h2><?php _e( 'Error 404 - Not Found', 'organized-docs' ); ?></h2>
+			<?php 
+			endif;
 
 		} // end check for main docs archive page
 		
@@ -111,15 +117,12 @@ if ( have_posts() ) :
 				foreach ( $postlist as $single_post ) {
 					echo '<li><a href="'.get_permalink($single_post->ID).'" title="' . esc_attr( $single_post->post_title ) .'">'.$single_post->post_title.'</a></li>';   
 				}  
+
 		        echo '</ul>';
 
 			} // end foreach ( $sorted_termchildren as $child_id => $order )
 
 		}// end check for empty $termchildren
-		else : ?>
-			<h2><?php _e( 'Error 404 - Not Found', 'organized-docs' ); ?></h2>
-		<?php 
-		endif;
 		do_action( 'organized_docs_content_bottom' ); ?>
 </div><!-- .entry-content -->
 </article>
