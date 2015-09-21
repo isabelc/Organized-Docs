2f5211a2f28f3422ceb9ec6d0d73c1c867a06776
Isabel Castillo <me@isabelcastillo.com>
2015-09-21 13:28:58 -0400

    list ALL posts in sidebar even if no child term.

--- includes/widget.php
+++ includes/widget.php
@@ -47,67 +47,103 @@ class DocsSectionContents extends WP_Widget {
 		$curr_term_id = $first_term->term_id;
 		$top_level_parent_term_id = $Isa_Organized_Docs->isa_term_top_parent_id( $curr_term_id );
 
+		// orderby custom option
+		$single_sort_by = get_option('od_single_sort_by');
+		$orderby_order = get_option('od_single_sort_order');
+				
+		if ( 'date' == $single_sort_by ) {
+			$orderby = 'date';
+		} elseif ( 'title - alphabetical' == $single_sort_by ) {
+			$orderby = 'title';
+		} else {
+			$orderby = 'meta_value_num';
+		}
+
 		// get term children
 		$termchildren =  get_term_children( $top_level_parent_term_id, 'isa_docs_category' );
-		
-		// sort $termchildren by custom subheading_sort_order numbers
-		$sorted_termchildren = $Isa_Organized_Docs->sort_terms_custom( $termchildren, 'subheading_sort_order' );
+ 
+		if ( empty($termchildren) ) {
+			// there are no child terms, do regular term loop to list ALL posts within current term
+			$query_args = array(
+						'post_type' => 'isa_docs', 
+						'posts_per_page' => -1,
+						'tax_query' => array(
+								array(
+									'taxonomy' => 'isa_docs_category',
+									'field' => 'id',
+									'terms' => $curr_term_id
+								)
+							),
+						'orderby' => $orderby,
+						'meta_key' => '_odocs_meta_sortorder_key',
+						'order' => $orderby_order
+			);
+			$the_query = new WP_Query( $query_args );
+			if ( $the_query->have_posts() ) : ?>
 
-		$list_each = get_option('od_widget_list_toggle');
-
-		if ($sorted_termchildren) {
-
-			foreach ( $sorted_termchildren as $child_id => $order ) {
-				$termobject = get_term_by( 'id', $child_id, 'isa_docs_category' );
-				
-				//Display the sub Term information, in open widget container ?>
-				<aside class="widget well"><h3 class="widget-title docs-sub-heading"><?php echo $termobject->name; ?></h3><?php
-				
-				// only list all posts if not disabled with setting
-				if( $list_each != 'hide' ) { ?>
-				<ul<?php
-				if ( 'toggle' == $list_each ) {
-					echo ' style="display:none"';
-				}
-				?>><?php
-				// nest a loop through each child cat's posts
-				global $post;
-				// orderby custom option
-				$single_sort_by = get_option('od_single_sort_by');
-				$orderby_order = get_option('od_single_sort_order');
-				
-				if ( 'date' == $single_sort_by ) {
-					$orderby = 'date';
-				} elseif ( 'title - alphabetical' == $single_sort_by ) {
-					$orderby = 'title';
-				} else {
-					$orderby = 'meta_value_num';
-				}
-				$query_args = array(	'post_type' => 'isa_docs', 
-							'posts_per_page' => -1,
-							'order' => 'ASC',
-							'tax_query' => array(
-										array(
-											'taxonomy' => 'isa_docs_category',
-											'field' => 'id',
-											'terms' => $termobject->term_id
-											)
-										),
-							'orderby' => $orderby,
-							'meta_key' => '_odocs_meta_sortorder_key',
-							'order' => $orderby_order
-					);
-				$postlist = get_posts( $query_args );
-				foreach ( $postlist as $single_post ) {
-						echo '<li';
-						if( $single_post->ID == $current_single_postID ) 
+				<aside class="widget well"><ul>
+				<?php while ( $the_query->have_posts() ) {
+					$the_query->the_post();
+					echo '<li';
+						if( $post->ID == $current_single_postID ) 
 							echo ' class="organized-docs-active-side-item"';
-						echo '><a href="' . get_permalink( $single_post->ID ) . '" title="' . esc_attr( $single_post->post_title ) . '">' . $single_post->post_title . '</a></li>';   
+						echo '><a href="' . get_permalink( $post->ID ) . '" title="' . esc_attr( $post->post_title ) . '">' . $post->post_title . '</a></li>';   
+
 				} ?>
-				</ul>
-				<?php } ?>
-				</aside><?php
+				</ul></aside><?php
+			endif;
+			wp_reset_postdata();
+		} else {
+			// sort $termchildren by custom subheading_sort_order numbers
+			$sorted_termchildren = $Isa_Organized_Docs->sort_terms_custom( $termchildren, 'subheading_sort_order' );
+
+			$list_each = get_option('od_widget_list_toggle');
+
+			if ($sorted_termchildren) {
+
+				foreach ( $sorted_termchildren as $child_id => $order ) {
+					$termobject = get_term_by( 'id', $child_id, 'isa_docs_category' );
+					
+					//Display the sub Term information, in open widget container ?>
+					<aside class="widget well"><h3 class="widget-title docs-sub-heading"><?php echo $termobject->name; ?></h3><?php
+					
+					// only list all posts if not disabled with setting
+					if( $list_each != 'hide' ) { ?>
+					<ul<?php
+					if ( 'toggle' == $list_each ) {
+						echo ' style="display:none"';
+					}
+					?>><?php
+					// nest a loop through each child cat's posts
+					global $post; // @test need
+
+					$query_args = array(	'post_type' => 'isa_docs', 
+								'posts_per_page' => -1,
+								'order' => 'ASC',
+								'tax_query' => array(
+											array(
+												'taxonomy' => 'isa_docs_category',
+												'field' => 'id',
+												'terms' => $termobject->term_id
+												)
+											),
+								'orderby' => $orderby,
+								'meta_key' => '_odocs_meta_sortorder_key',
+								'order' => $orderby_order
+						);
+					$postlist = get_posts( $query_args );
+					foreach ( $postlist as $single_post ) {
+							echo '<li';
+							if( $single_post->ID == $current_single_postID ) 
+								echo ' class="organized-docs-active-side-item"';
+							echo '><a href="' . get_permalink( $single_post->ID ) . '" title="' . esc_attr( $single_post->post_title ) . '">' . $single_post->post_title . '</a></li>';   
+					} ?>
+					</ul>
+					<?php } ?>
+					</aside><?php
+				}
 			}
+
 		}
 		echo $args['after_widget'];
 		if ( 'toggle' == $list_each ) {
